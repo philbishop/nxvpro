7ba508c64379d51036ee4658c2a372dd99f882fb
Phil Bishop <info@howfar.uk>
2022-02-09 15:39:57 +0000

    Initial Commit

---
+++ nxvpro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nxvpro
+//
+//  Created by Philip Bishop on 09/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nxvpro/nxvproApp.swift
@@ -0,0 +1,17 @@
+//
+//  nxvproApp.swift
+//  nxvpro
+//
+//  Created by Philip Bishop on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct nxvproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
